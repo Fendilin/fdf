50b31910b06ef6ec292e3c77e517cde3214bdd35
Valentin TERZIAN <vterzian@e3r12p9.42.fr>
2014-12-10 19:41:47 +0100

    Update

--- fdf.h
+++ fdf.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vterzian <vterzian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/10 13:25:22 by vterzian          #+#    #+#             */
+/*   Updated: 2014/12/10 14:54:28 by vterzian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
@@ -6,11 +17,20 @@
 # include "get_next_line.h"
 # include <fcntl.h>
 
+typedef struct s_line
+{
+	int *tabint;
+	int size;
+
+	struct s_line *next;
+}	t_line;
+
 typedef struct s_coord
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 	int z;
+
 	struct s_coord *next;
-}	t_coord;
+}	t_coord
 #endif
--- main.c
+++ main.c
@@ -1,116 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vterzian <vterzian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/10 13:25:27 by vterzian          #+#    #+#             */
+/*   Updated: 2014/12/10 19:41:28 by vterzian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "fdf.h"
 
-static int		ft_fdf_count_columns(char *file)
+static int	ft_fdf_count_lines(fd)
 {
-	int		fd;
 	int		i;
 	char	*str;
-	char	**split;
 
-	i = 0;
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	get_next_line(fd, &str);
-	split = ft_strsplit(str, ' ');
-	ft_strdel(&str);
-	while (split[i] != NULL)
-		i++;
-	free(split);
-	close(fd);
-	return (i);
-}
-
-static int		ft_fdf_count_lines(char *file)
-{
-	int 	fd;
-	int 	i;
-	int		j;
-	char	*str;
-
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (-1);
-	j = 0;
 	while ((i = get_next_line(fd, &str)) > 0)
 	{
-		j++;
-		ft_strdel(&str);
+		
 	}
-	if (i == -1)
-		return(-1);
-	close(fd);
-	return (j);
 }
 
-static int	**ft_fdf_get_map(char *file)
+static s_line 	ft_fdf_get_map(int fd)
 {
-	int		nbr_lines;
-	int		nbr_columns;
-	int		fd;
 	char	*str;
-	char	**split;
-	int		**map;
-	int		y;
-	int		x;
-
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	y = 0;
-	x = 0;
-	nbr_lines = ft_fdf_count_lines(file);
-	nbr_columns = ft_fdf_count_columns(file);
-	map = (int**)malloc(sizeof(int**) * nbr_lines);
-	*map = (int*)malloc(sizeof(int*) * nbr_columns);
-	while (y < nbr_lines)
+	int		*tab;
+	int		nbr_lines;
+	s_line	tab;
+	
+	tab = NULL;
+	map = ft_fdf_get_line(str);
+	while ()
 	{
-		get_next_line(fd, &str);
-		split = ft_strsplit(str, ' ');
-		ft_strdel(&str);
-		while (x < nbr_columns)
-		{
-			map[y][x] = ft_atoi(split[x]);
-			ft_putnbr(map[y][x]);
-			ft_putstr("  ");
-			free(split[x]);
-			x++;
-		}
-		free(split);
-		ft_putchar('\n');
-		y++;
-		x = 0;
+
 	}
-	close(fd);
-	return (map);
+	return (tab);
 }
 
-int						main(int argc, char **argv)
+int				main(int argc, char **argv)
 {
 	(void)argc;
-	char	*file;
-	int	**map;
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
+	s_line line;
+	
+	line = NULL;
 	if (argv[1] == NULL)
 		return (-1);
-	file = argv[1];
-	map = ft_fdf_get_map(file);
-	while (i < ft_fdf_count_lines(file))
-	{
-		while (j < ft_fdf_count_columns(file))
-		{
-			ft_putnbr(map[i][j]);
-			ft_putchar(' ');
-			j++;
-		}
-		ft_putchar('\n');
-		i++;
-		j = 0;
-	}
+	fd = open(argv[1]);
+	if (fd < 0)
+		return (-1);
+	line.tabint = ft_fdf_get_map(fd);
 	return (0);
 }
